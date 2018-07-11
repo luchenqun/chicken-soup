89f1149eeb5d4e633af77946e025457a1fef63a9
luchenqun <luchenqun@juzix.io>
2018-07-11 11:09:13 +0800

    后台去掉import使用require

--- server/app.js
+++ server/app.js
@@ -1,23 +1,17 @@
-import Koa from 'koa';
-import path from 'path';
-import serve from 'koa-static';
-import bodyparser from 'koa-bodyparser';
-
-import api from './routes/api';
+let Koa = require("koa");
+let path = require("path");
+let serve = require("koa-static");
+let bodyparser = require("koa-bodyparser");
+let api = require("./routes/api");
 
 const app = new Koa();
 
-app.use(async ctx => {
-  ctx.body = 'i love this world';
-});
-
 app.use(
-  serve(path.resolve(__dirname, '../dist'), {
-    maxage: 1000 * 60 * 60 * 24 * 30, // a month
-  }),
+  serve(path.resolve(__dirname, "../dist"), {
+    maxage: 1000 * 60 * 60 * 24 * 30 // a month
+  })
 );
 app.use(bodyparser());
 app.use(api());
-
 
 app.listen(8000);
--- server/routes/api.js
+++ server/routes/api.js
@@ -1,14 +1,14 @@
-import Router from 'koa-router';
-
-export default function () {
+var Router = require("koa-router");
+var api = function() {
   const router = Router();
 
-  router.prefix('/api');
+  router.prefix("/api");
 
-  router.get('/hello', async (ctx) => {
-    ctx.body = 'Hello Vue';
+  router.get("/hello", async ctx => {
+    ctx.body = "Hello Vue";
   });
 
   return router.routes();
+};
 
-}
+module.exports = api;
