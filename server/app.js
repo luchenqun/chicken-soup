4944ff2148a504774a0c7e3fe62d60c773a9f2eb
luchenqun <luchenqun@juzix.io>
2019-01-09 13:20:23 +0800

    更新爬虫方式

--- server/app.js
+++ server/app.js
@@ -37,110 +37,131 @@ let items = config.items;
 
 // http://www.360wa.com/m/duanxiaohua/latest
 // http://www.360wa.com/duanxiaohua/product/336394
-
+// http://www.360wa.com/m/classics
 let spidering = false;
-let index = 0;
+let types = {};
+for (item of items) {
+  types[item.pinyin] = item.type;
+}
 setInterval(async () => {
   if (spidering) return;
   spidering = true;
-  let item = items[index % items.length];
-  let pid = item.id;
-  let url = `http://www.360wa.com/${item.pinyin}/product/${item.id}`;
-
   try {
+    const url = `http://www.360wa.com/m/classics`;
     let body = await rp(url);
     let $ = cheerio.load(body, {
       decodeEntities: false
     });
 
-    let title = $(".title")
-      .text()
-      .trim(); // 标题
-    let date = $(".ft")
-      .text()
-      .trim(); // 创建日期
+    let jokes = $(".list");
+    for (let i = 0; i < jokes.length; i++) {
+      let joke = jokes[i];
+      // 链接
+      let href = $(joke)
+        .children(".cls-point")
+        .find("a")
+        .attr("href");
 
-    let $content = $(".article-content").eq(0);
-    let text = $content.text().trim(); // 正文
+      if (!href) continue;
 
-    let imgs = []; // 正文里面包含的图片
-    $content.children("img").each(function(i, elem) {
-      let src = $(this).attr("src");
-      imgs.push(src);
-    });
+      let hrefArr = href.split("/");
+      let pid = hrefArr[hrefArr.length - 1];
+      let type = types[hrefArr[hrefArr.length - 3]];
 
-    let comments = []; // 评论
-    $(".comment").each(function(i, elem) {
-      if (i == 0) return;
-      let comment = $(this)
-        .children("p")
+      // 标题
+      let title = $(joke)
+        .children(".cls-point")
+        .find("h1")
         .text()
         .trim();
-      let date = $(this)
-        .children(".meta")
+
+      // 内容
+      let text = $(joke)
+        .children(".cls-point")
+        .find("p")
         .text()
         .trim();
-      comments.push({
-        joke_id: pid,
+
+      // 点赞数(当查看数)
+      let see = parseInt(
+        $(joke)
+          .children(".point-user")
+          .find("#" + pid)
+          .text()
+          .trim()
+      );
+
+      // 正文里面包含的图片
+      let imgs = [];
+      $(joke)
+        .children(".cls-point")
+        .find("img")
+        .each(function() {
+          imgs.push($(this).attr("src"));
+        });
+
+      if (title) {
+        title = "<h1>" + title + "</h1>";
+      }
+
+      let content = (title + (title && text && "<br/>") + "<p>" + text + "</p>").replace(/\n/g, "<br/>");
+
+      let data = {
+        pid: pid,
         user_id: 1,
-        link_id: 1,
-        date: date,
-        comment: comment,
-        type: 1
-      });
-    });
+        content: content,
+        type: type,
+        imgs: JSON.stringify(imgs),
+        see: see
+      };
 
-    let nextHref = $(".forum2").attr("href");
-    item.id = nextHref.substring(nextHref.lastIndexOf("/") + 1); // 下一个爬取的id
-
-    let see = $(`#${pid}`)
-      .text()
-      .trim(); // 创建日期
-
-    let content = (title + (title && text && "<br/>") + text).replace(/\n/g, "<br/>");
-
-    let data = {
-      pid: pid,
-      user_id: 1,
-      content: content,
-      type: item.type,
-      created_at: date,
-      imgs: JSON.stringify(imgs),
-      see: parseInt(see) + 1
-    };
-
-    // console.log(pid, JSON.stringify(content), item.type, date, JSON.stringify(imgs), see + 1, comments);
-
-    // 插段子
-    let joke = await Jokes.findOne({
-      where: { pid },
-      raw: true
-    });
-    if (joke) {
-      console.log("joke database already hava : ", url);
-    } else {
-      await Jokes.create(data);
-    }
-
-    // 插段子评论
-    for (const comment of comments) {
-      let link = await Links.findOne({
-        where: comment,
+      // 插段子
+      let jokeFind = await Jokes.findOne({
+        where: { pid },
         raw: true
       });
-      if (link) {
-        console.log("commit database already hava : ", comment.comment);
+      if (jokeFind) {
+        console.log("joke database already hava, pid = ", pid);
       } else {
-        await Links.create(comment);
+        await Jokes.create(data);
+      }
+
+      // 爬评论
+      const commitUrl = `http://www.360wa.com/topic!ajaxViewComments.html?topicId=${pid}`;
+      body = await rp(commitUrl);
+      $ = cheerio.load(body, {
+        decodeEntities: false
+      });
+
+      let comments = $(".left2"); // 评论
+      for (let i = 0; i < comments.length; i++) {
+        let text = $(comments[i])
+          .text()
+          .replace(/\s/g, "");
+        text = text.substring(text.lastIndexOf(")") + 1);
+        if (text) {
+          let comment = {
+            joke_id: pid,
+            user_id: 1,
+            link_id: 1,
+            comment: text,
+            type: 1
+          };
+          let link = await Links.findOne({
+            where: comment,
+            raw: true
+          });
+          if (link) {
+            console.log("commit database already hava : ", comment.comment);
+          } else {
+            await Links.create(comment);
+          }
+        }
       }
     }
   } catch (error) {
-    console.log(url, "error", error);
+    console.log("error", error);
   }
 
   spidering = false;
-  index++;
-  if (index % 200 === 0) {
-    fs.writeFileSync(path.join(__dirname, "config.json"), JSON.stringify(config));
-  }
-}, 3000);
+}, 9000);
