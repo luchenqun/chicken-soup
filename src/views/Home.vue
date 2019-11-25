4945b08d21ec64fe2adfa78eb3eb3d423e319083
HelloWorld <helloworld@qq.com>
2019-11-25 20:42:34 +0800

    使用搜狗缓存图片

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -189,7 +189,7 @@ export default {
       let resp = await axios.get("/api/joke/", { params });
       this.joke = resp.data.joke;
       for (let index = 0; index < this.joke.imgs.length; index++) {
-        this.joke.imgs[index] = `http://img01.store.sogou.com/net/a/04/link?appid=100520029&url=${this.joke.imgs[index]}`;
+        this.joke.imgs[index] = `https://images.weserv.nl/?url=${this.joke.imgs[index]}`;
       }
       this.user = {};
       for (let user of this.users) {
